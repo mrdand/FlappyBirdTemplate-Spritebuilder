c299bcc9e8f22c674d2afbd8d703a0c527351164
mrdand <agustinohalim@gmail.com>
2015-04-15 13:21:18 +0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -11,11 +11,19 @@
     [physicsNode addChild:character];
     
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacle += delta;
+    if (timeSinceObstacle > 2.0f)
+    {
+        [self addObstacle];
+        timeSinceObstacle = 0.0f;
+    }
+    
 }
 
 // put new methods here
