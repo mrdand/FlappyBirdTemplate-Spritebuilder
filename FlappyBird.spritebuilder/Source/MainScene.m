f497eb758cc73e380a8577bf639262fd5914d0e7
mrdand <agustinohalim@gmail.com>
2015-04-16 19:31:45 +0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -173,6 +173,22 @@
         [_obstacles removeObject:obstacleToRemove];
     }
     
+    for (CCNode *bush in _bushes) {
+        bush.position = ccp(bush.position.x - (character.physicsBody.velocity.x * delta), bush.position.y);
+        
+        if (bush.position.x <= (-1 * bush.contentSize.width)) {
+            bush.position = ccp(bush.position.x + 2 * bush.contentSize.width, bush.position.y);
+        }
+    }
+    
+    for (CCNode *cloud in _clouds) {
+        cloud.position = ccp(cloud.position.x - (character.physicsBody.velocity.x * delta), cloud.position.y);
+        
+        if (cloud.position.x <= (-1 * cloud.contentSize.width)) {
+            cloud.position = ccp(cloud.position.x + 2 * cloud.contentSize.width, cloud.position.y);
+        }
+    }
+    
 
     if (!_gameOver)
     {
